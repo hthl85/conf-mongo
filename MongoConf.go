97daaf4864995ee4c6e66de2adb66cbe02c19146
Le Trong <le.trong>
2020-03-02 22:49:49 -0500

    Formatted code

--- MongoConf.go
+++ MongoConf.go
@@ -2,13 +2,13 @@ package mongoconf
 
 // MongoConf struct
 type MongoConf struct {
-	Host          string
-	Username      string
-	Password      string
-	Dbname        string
-	Port          uint64
-	MinPoolSize   uint64
-	MaxPoolSize   uint64
-	MaxIdleTimeMS uint64
-	Timeout       int
+    Host          string
+    Username      string
+    Password      string
+    Dbname        string
+    Port          uint64
+    MinPoolSize   uint64
+    MaxPoolSize   uint64
+    MaxIdleTimeMS uint64
+    Timeout       int
 }
