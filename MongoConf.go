7ad0b4b955e12cc4b2c49517cf84fc7f6348b038
Le Trong <le.trong>
2020-03-02 22:31:43 -0500

    Init mongo config

---
+++ MongoConf.go
@@ -0,0 +1,16 @@
+package mongoconf
+
+import "time"
+
+// MongoConf struct
+type MongoConf struct {
+	Host          string
+	Username      string
+	Password      string
+	Dbname        string
+	Port          uint64
+	MinPoolSize   uint64
+	MaxPoolSize   uint64
+	MaxIdleTimeMS uint64
+	Timeout       time.Duration
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hthl85/conf-mongo
+
+go 1.14
